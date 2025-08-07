c11dc8f04d965577ea5e99796645bf588160da36
prasad <hackerspider09@gmail.com>
2025-08-07 20:03:48 +0530

    hostname api added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import socket
 
 app = Flask(__name__)
 
@@ -10,6 +11,15 @@ def health_check():
 def greet():
     name = request.args.get('name', 'World')
     return jsonify({"message": f"Hello, {name}!"}), 200
+
+
+@app.route('/host', methods=['GET'])
+def getHost():
+    # time.sleep(2)
+    s= socket.gethostname()
+    ip_address = socket.gethostbyname(s)
+
+    return jsonify(f"Hello Host name : {s}, Host addr : {ip_address}")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
